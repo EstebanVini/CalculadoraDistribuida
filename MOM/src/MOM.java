191f673dbb85fe36bab91732f9584014a5f9a7ff
Esteban Viniegra <0235320@up.edu.mx>
2023-10-29 22:19:35 -0600

    Ya casi es posible comunicar todos los middelwares

--- MOM/src/MOM.java
+++ MOM/src/MOM.java
@@ -1,35 +1,34 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import java.util.HashSet;
 import java.util.Set;
 
 public class MOM {
-    private List<Integer> puertosDisponibles;
+    private List<Integer> availablePorts;
     private List<Socket> middlewareSockets;
     private List<DataOutputStream> middlewareSalidas;
     private ServerSocket servidor;
     private Set<String> mensajesConocidos;
 
     public MOM(List<Integer> availablePorts) {
-        puertosDisponibles = new ArrayList<>(availablePorts);
+        this.availablePorts = availablePorts;
         middlewareSockets = new ArrayList<>();
         middlewareSalidas = new ArrayList<>();
         mensajesConocidos = new HashSet<>();
     }
 
     public void startMiddleware() {
-        while (!puertosDisponibles.isEmpty()) {
-            int index = new Random().nextInt(puertosDisponibles.size());
-            int port = puertosDisponibles.remove(index);
+        while (!availablePorts.isEmpty()) {
+            int index = new Random().nextInt(availablePorts.size());
+            int port = availablePorts.remove(index);
 
             try {
                 servidor = new ServerSocket(port);
                 System.out.println("Middleware en puerto " + port + " está corriendo");
-                startMiddlewareInstance(servidor);
-                establishMiddlewareConnections();
+                startMiddlewareInstance(servidor, port);
                 return; // Exit after successfully starting a middleware
             } catch (IOException e) {
                 System.out.println("Error al intentar iniciar en puerto " + port + ": " + e);
@@ -39,7 +38,7 @@ public class MOM {
         System.out.println("No se pudo iniciar el middleware en ningún puerto disponible.");
     }
 
-    private void startMiddlewareInstance(ServerSocket servidor) {
+    private void startMiddlewareInstance(ServerSocket servidor, int myPort) {
         List<ManejadorDeClientes> clientes = new ArrayList<>();
 
         new Thread(() -> {
@@ -53,6 +52,13 @@ public class MOM {
 
                     Thread thread = new Thread(clientHandler);
                     thread.start();
+
+                    // Establecer una conexión bidireccional con el middleware anterior en el anillo
+                    int previousPort = myPort - 1;
+                    if (previousPort <= 0) {
+                        previousPort += availablePorts.size();
+                    }
+                    connectToPreviousMiddleware(previousPort);
                 }
             } catch (IOException e) {
                 System.out.println(e);
@@ -60,57 +66,28 @@ public class MOM {
         }).start();
     }
 
+    private void connectToPreviousMiddleware(int previousPort) {
+        if (middlewareSockets.size() < 2) {
+            try {
+                Socket socket = new Socket("127.0.0.1", previousPort);
+                middlewareSockets.add(socket);
+                DataOutputStream salidaMiddleware = new DataOutputStream(socket.getOutputStream());
+                middlewareSalidas.add(salidaMiddleware);
+                System.out.println("Conexión establecida con otro Middleware en puerto " + previousPort);
+            } catch (IOException e) {
+                System.out.println("Error al intentar conectar al Middleware en puerto " + previousPort + ": " + e);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         List<Integer> availablePorts = new ArrayList<>();
         availablePorts.add(12345);
         availablePorts.add(12346);
-        availablePorts.add(12347); // Agrega los puertos disponibles aquí
+        availablePorts.add(12347);// Agrega los puertos disponibles aquí
 
         MOM server = new MOM(availablePorts);
         server.startMiddleware();
-    }
-
-    private void establishMiddlewareConnections() {
-        for (int port : puertosDisponibles) {
-            if (port != servidor.getLocalPort()) {
-                try {
-                    Socket socket = new Socket("127.0.0.1", port);
-                    middlewareSockets.add(socket);
-                    DataOutputStream salidaMiddleware = new DataOutputStream(socket.getOutputStream());
-                    middlewareSalidas.add(salidaMiddleware);
-                    System.out.println("Conexión establecida con otro Middleware en puerto " + port);
-                } catch (IOException e) {
-                    System.out.println("Error al intentar conectar a otro Middleware en puerto " + port + ": " + e);
-                }
-            }
-        }
-    }
-
-    private boolean isMiddleware(Socket socket) {
-        try {
-            // Configura un mensaje de autenticación específico para middlewares
-            String mensajeAutenticacion = "AUTENTICACION_MOM";
-
-            // Abre flujos de entrada y salida para el socket
-            DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
-            DataInputStream entrada = new DataInputStream(socket.getInputStream());
-
-            // Envía el mensaje de autenticación al nuevo socket
-            salida.writeUTF(mensajeAutenticacion);
-
-            // Espera una respuesta del nuevo socket
-            String respuesta = entrada.readUTF();
-
-            // Comprueba si la respuesta coincide con un mensaje de autenticación esperado
-            if (respuesta.equals(mensajeAutenticacion)) {
-                return true; // El socket conectado es otro middleware
-            }
-        } catch (IOException e) {
-            // Manejar cualquier error de E/S que ocurra al intentar autenticar
-            e.printStackTrace();
-        }
-
-        return false; // El socket conectado no es otro middleware
     }
 
     private class ManejadorDeClientes implements Runnable {
@@ -125,25 +102,24 @@ public class MOM {
         @Override
         public void run() {
             try {
-                DataInputStream entrada = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                DataInputStream entrada = new DataInputStream(socket.getInputStream());
 
-                String temp = "";
                 while (true) {
-                    temp = entrada.readUTF();
+                    String mensaje = entrada.readUTF();
 
                     // Verifica si el mensaje ya se conoce y evita su reenvío
-                    if (!mensajesConocidos.contains(temp)) {
-                        mensajesConocidos.add(temp);
+                    if (!mensajesConocidos.contains(mensaje)) {
+                        mensajesConocidos.add(mensaje);
 
                         // Enviar el mensaje a todos los demás clientes y middlewares.
                         for (ManejadorDeClientes client : clientes) {
                             if (client != this) {
                                 try {
                                     DataOutputStream clientSalida = new DataOutputStream(client.socket.getOutputStream());
-                                    clientSalida.writeUTF(temp);
+                                    clientSalida.writeUTF(mensaje);
                                 } catch (IOException e) {
                                     // Manejar la excepción si falla el envío a un cliente.
-                                    e.printStackTrace();
+                                    System.out.println("Error al enviar a un cliente: " + e);
                                 }
                             }
                         }
@@ -151,16 +127,22 @@ public class MOM {
                         // Enviar el mensaje a todos los middlewares
                         for (DataOutputStream salidaMiddleware : middlewareSalidas) {
                             try {
-                                salidaMiddleware.writeUTF(temp);
+                                salidaMiddleware.writeUTF(mensaje);
                             } catch (IOException e) {
                                 // Manejar la excepción si falla el envío a un middleware.
-                                e.printStackTrace();
+                                System.out.println("Error al enviar a un middleware: " + e);
                             }
                         }
                     }
                 }
-            } catch (IOException e) {
-                System.out.println(e);
+            } catch (IOException error) {
+                System.out.println("Error de comunicación con el cliente: " + error);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println("Error al cerrar el socket: " + e);
+                }
             }
         }
     }
